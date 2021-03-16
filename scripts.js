4000a3b74344e2d7019d8bd6957e84d318323b89
NamPham <vannam1329@gmail.com>
2021-03-16 09:53:37 +0700

    push file

---
+++ scripts.js
@@ -0,0 +1,23 @@
+var input=document.querySelector('.result');
+var items=Array.from(document.querySelectorAll('.item'));
+items.forEach(function(btn){
+    btn.addEventListener('click',function(){
+        if(input.innerHTML=='0')
+        input.innerHTML='';
+        if(btn.innerHTML=='AC')
+        input.innerHTML='';
+        else if(btn.innerHTML=='='){
+            input.innerHTML=eval(input.innerHTML);
+        }
+        else if(btn.innerHTML=='+/-'){
+            input.innerHTML=(input.innerHTML)*(-1);
+        }                
+        else if(this.id=='x'){
+            input.innerHTML=(input.innerHTML)+'*';
+        }
+        else if(this.id==','){
+            input.innerHTML=(input.innerHTML)+'.';
+        }
+        else input.innerHTML+=btn.innerHTML;
+    })
+})
