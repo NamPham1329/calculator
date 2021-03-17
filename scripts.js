4aa708613b8933557d9b60dff55065d4537e5df7
NamPham <vannam1329@gmail.com>
2021-03-17 09:26:25 +0700

    fđf

--- scripts.js
+++ scripts.js
@@ -3,28 +3,31 @@ var items=Array.from(document.querySelectorAll('.item'));
 var x='*';
 items.forEach(function(btn){
     btn.addEventListener('click',function(){
-        if(input.innerHTML=='0')
+        if(input.innerHTML=='zero')
         input.innerHTML='';
-        if(btn.innerHTML=='AC')
+        if(this.id=='delete_all')
         input.innerHTML='';
-        else if(btn.innerHTML=='='){
+        else if(this.id=='result'){
             input.innerHTML=eval(input.innerHTML);
         }
-        else if(btn.innerHTML=='+/-'){
+        else if(this.id=='negative'){
             input.innerHTML=(input.innerHTML)*(-1);
         }          
-        else if(this.id=='x'){
+        else if(this.id=='multiplication'){
             input.innerHTML=(input.innerHTML)+'*';
         }
-        else if(this.id=='%'){
+        else if(this.id=='remainder'){
             input.innerHTML=(input.innerHTML)/100;
         }
-        else if(this.id==','){
+        else if(this.id=='comma'){
             input.innerHTML=(input.innerHTML)+'.';
         }
-        else if(this.id=='÷'){
+        else if(this.id=='division'){
             input.innerHTML=(input.innerHTML)+'/';
         }
+        // else if(input.innerHTML="0"+btn.innerHTML){
+        //     return input.innerHTML;
+        // }
         else input.innerHTML+=btn.innerHTML;
     })
 })
