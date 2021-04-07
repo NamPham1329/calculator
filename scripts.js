60dcb123f7ac19b71b38d115e100073a83b72051
NamPham <vannam1329@gmail.com>
2021-04-07 09:56:25 +0700

    check lỗi số 0

--- scripts.js
+++ scripts.js
@@ -1,10 +1,7 @@
 var input=document.querySelector('.content');
 var items=Array.from(document.querySelectorAll('.item'));
-var x='*';
 items.forEach(function(btn){
     btn.addEventListener('click',function(){
-        if(input.innerHTML=='zero')
-        input.innerHTML='';
         if(this.id=='delete_all')
         input.innerHTML='';
         else if(this.id=='result'){
@@ -25,9 +22,6 @@ items.forEach(function(btn){
         else if(this.id=='division'){
             input.innerHTML=(input.innerHTML)+'/';
         }
-        // else if(input.innerHTML="0"+btn.innerHTML){
-        //     return input.innerHTML;
-        // }
         else input.innerHTML+=btn.innerHTML;
     })
 })
